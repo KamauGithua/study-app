6a9536453d6b6b3b2714f1e2b382cc2cefa28c11
kamau <githuakamau99@gmail.com>
2024-10-11 11:17:50 +0300

    created a private function SubjectCountCardSection

--- app/src/main/java/com/kamau/studyapp/presentation/dashboard/DashboardScreen.kt
+++ app/src/main/java/com/kamau/studyapp/presentation/dashboard/DashboardScreen.kt
@@ -1,5 +1,6 @@
 package com.kamau.studyapp.presentation.dashboard
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -21,19 +23,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.core.app.NotificationCompat.Style
+import com.kamau.studyapp.R
 import com.kamau.studyapp.domain.model.Subject
 import com.kamau.studyapp.presentation.components.CountCard
 
 @Composable
-fun DashboardScreen(){
+fun DashboardScreen() {
     Scaffold(
         topBar = { DashboardScreenTopBar() }
     ) { paddingValues ->
-        LazyColumn(modifier = Modifier
-            .fillMaxSize()
-            .padding(paddingValues)
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
         ) {
             item {
                 CountCardsSection(
@@ -45,6 +51,12 @@ fun DashboardScreen(){
                     goalHours = "15"
                 )
             }
+            item {
+                SubjectCountCardSection(
+                    modifier = Modifier.fillMaxWidth(),
+                    subjectList = emptyList()
+                )
+            }
         }
 
     }
@@ -52,11 +64,15 @@ fun DashboardScreen(){
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun DashboardScreenTopBar(){
-    CenterAlignedTopAppBar(title = {
-        Text(text = "Study App",
-            style = MaterialTheme.typography.headlineMedium)
-    })
+private fun DashboardScreenTopBar() {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = "Study App",
+                style = MaterialTheme.typography.headlineMedium
+            )
+        }
+    )
 }
 
 @Composable
@@ -94,24 +110,43 @@ private fun CountCardsSection(
 @Composable
 private fun SubjectCountCardSection(
     modifier: Modifier,
-    subjectList: List<Subject>
-    ){
-    Column(modifier= modifier) {
-        Row (modifier = Modifier
-            .fillMaxWidth(),
+    subjectList: List<Subject>,
+    emptyListText: String = "You don't have any subjects. \n Click the + button to add new subject "
+) {
+    Column(modifier = modifier) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
-        ){
-            Text(text = "SUBJECTS",
+        ) {
+            Text(
+                text = "SUBJECTS",
                 style = MaterialTheme.typography.bodySmall,
-                modifier = Modifier.padding(start = 12.dp))
+                modifier = Modifier.padding(start = 12.dp)
+            )
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Subject"
+                )
+            }
         }
-        IconButton(onClick = { /*TODO*/ }) {
-            Icon(
-                imageVector = Icons.Default.Add,
-                contentDescription = "Add Subject"
+
+        if (subjectList.isEmpty()) {
+            Image(
+                painter = painterResource(R.drawable.img_books),
+                contentDescription = emptyListText,
+                modifier = Modifier
+                    .size(120.dp)
+                    .align(Alignment.CenterHorizontally) // Proper use of align
+            )
+            Text(
+                text = emptyListText,
+                style = MaterialTheme.typography.bodySmall,
+                color = Color.Gray,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.align(Alignment.CenterHorizontally) // Aligns text in the center
             )
         }
     }
-
 }
